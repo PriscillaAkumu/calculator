8cb1a73bebdae1b23c3237d326699239841c0084
PriscillaAkumu <sulepriscilla@gmail.com>
2022-09-27 18:27:56 +0300

    added files

---
+++ calcFeatures/calcDefinitions/stepfs.js
@@ -0,0 +1,57 @@
+const assert = require("assert");
+const { Given, When, Then } = require("@cucumber/cucumber");
+
+// scenario 1
+Given("user inputs {int} numbers", function (int) {
+  // Given('user inputs {float} numbers', function (float) {
+  // Write code here that turns the phrase above into concrete actions
+  return "pending";
+});
+
+Then("return the sum of the {int} numbers", function (int) {
+  // Then('return the sum of the {float} numbers', function (float) {
+  // Write code here that turns the phrase above into concrete actions
+  return "pending";
+});
+
+// // scenario 2
+// Given("user inputs {int} numbers", function (int) {
+//   // Given('user inputs {float} numbers', function (float) {
+//   // Write code here that turns the phrase above into concrete actions
+//   return "pending";
+// });
+
+// Then("return the difference of the {int} numbers", function (int) {
+//   // Then('return the difference of the {float} numbers', function (float) {
+//   // Write code here that turns the phrase above into concrete actions
+//   return "pending";
+// });
+
+
+// // scenario 3
+//  Given("user inputs {int} numbers", function (int) {
+//    // Given('user inputs {float} numbers', function (float) {
+//    // Write code here that turns the phrase above into concrete actions
+//    return "pending";
+//  });
+
+//  Then("return the product of the {int} numbers", function (int) {
+//    // Then('return the product of the {float} numbers', function (float) {
+//    // Write code here that turns the phrase above into concrete actions
+//    return "pending";
+//  });
+
+
+//  // Scenario 4
+//  Given("user inputs {int} numbers", function (int) {
+//    // Given('user inputs {float} numbers', function (float) {
+//    // Write code here that turns the phrase above into concrete actions
+//    return "pending";
+//  });
+
+//  Then("return the division of the {int} numbers", function (int) {
+//    // Then('return the division of the {float} numbers', function (float) {
+//    // Write code here that turns the phrase above into concrete actions
+//    return "pending";
+//  });
+
